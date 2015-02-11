f7dcf7c1705dac0992eba7299601260364b11561
valentinchaput <valentin.chaput@gmail.com>
2015-02-11 16:59:39 +0100

    with basic bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'restaurants', to: 'restaurants#index'
+  root to: 'restaurants#index'
 
-  get 'restaurants/new', to: 'restaurants#new', as: 'new'
-  post 'restaurants', to: 'restaurants#create'
-
-  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
-
-  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
-  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
